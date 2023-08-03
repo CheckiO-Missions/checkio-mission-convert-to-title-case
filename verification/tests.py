e6a8a8199520a64745d8f086b4d0e23263c6aab7
Oleksandr Zozulia <freemanlex@gmail.com>
2023-08-03 20:48:35 +0300

    "first commit"

---
+++ verification/tests.py
@@ -0,0 +1,65 @@
+"""
+TESTS is a dict with all of your tests.
+Keys for this will be the categories' names.
+Each test is a dict with
+    "input" -- input data for a user function
+    "answer" -- your right answer
+    "explanation" -- not necessarily a key, it's used for an additional info in animation.
+"""
+
+
+TESTS = {
+    "Basics": [
+        {
+            "input": ["hello world"],
+            "answer": "Hello World"
+        },
+        {
+            "input": ["openai gpt-4"],
+            "answer": "Openai Gpt-4"
+        },
+        {
+            "input": ["this is a title"],
+            "answer": "This Is A Title"
+        },
+        {
+            "input": ["THE QUICK BROWN FOX"],
+            "answer": "The Quick Brown Fox"
+        },
+        {
+            "input": ["JUMPs ovER a LAZy dog"],
+            "answer": "Jumps Over A Lazy Dog"
+        },
+        {
+            "input": ["typescript is great"],
+            "answer": "Typescript Is Great"
+        },
+        {
+            "input": ["the answer is 42"],
+            "answer": "The Answer Is 42"
+        },
+        {
+            "input": ["to be or not to be"],
+            "answer": "To Be Or Not To Be"
+        },
+        {
+            "input": ["that is the question"],
+            "answer": "That Is The Question"
+        },
+        {
+            "input": [""],
+            "answer": ""
+        },
+    ],
+    "Extra": [
+        {
+            "input": ["a"],
+            "answer": "A"
+        },
+        {
+            "input": ["b C d"],
+            "answer": "B C D"
+        },
+    ]
+}
+
